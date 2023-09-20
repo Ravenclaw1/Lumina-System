46a648ec98bc41da6dfdd8c61e201c292958ea5a
Ravenclaw1 <63686003+Ravenclaw1@users.noreply.github.com>
2023-09-20 13:46:22 -0500

    Update luminasystem.py

--- luminasystem.py
+++ luminasystem.py
@@ -25,6 +25,7 @@ model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
 model_n_batch = int(os.environ.get('MODEL_N_BATCH', 8))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS', 4))
+model_temperature = float(os.environ.get('MODEL_TEMPERATURE', 0.5)) 
 
 # Import chroma settings from a constants module
 from constants import CHROMA_SETTINGS
@@ -44,12 +45,12 @@ def main():
 
     # Initialize the Language Learning Model (LLM) based on the specified model type
     match model_type:
-        case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-        case "GPT4All":
-            llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-        case _default:
-            raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
+    case "LlamaCpp":
+        llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False, temperature=model_temperature)  # Assuming LlamaCpp accepts a temperature argument
+    case "GPT4All":
+        llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False, temperature=model_temperature)  # Assuming GPT4All accepts a temperature argument
+    case _default:
+        raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
 
     # Initialize the QA chain
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=not args.hide_source)
